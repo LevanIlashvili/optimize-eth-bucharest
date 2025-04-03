db1993e416667a44810688b576653045746e030c
Levan Ilashvili <levan@ilashvili.me>
2025-04-03 19:25:14 +0300

    fixed failing test

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,8 @@ use alloc::vec::Vec;
 
 use libbucharesthashing::{immutables::*, prover, prover::Piece};
 
+use std::collections::HashSet;
+
 pub type Board = Vec<(u32, Piece, u32)>;
 
 fn pos_to_xy(row_size: u32, p: u32) -> (u32, u32) {
@@ -46,7 +48,6 @@ pub fn solve(starting_hash: &[u8], start: u32) -> Option<(u32, u32)> {
     let row_size = BOARD_SIZE.isqrt();
     let mut board: Board = Vec::new();
     let mut last_king: Option<(u32, u32)> = None; 
-    let mut threats: Vec<u32> = vec![];
 
     for i in start..MAX_TRIES {
         let e = prover::hash(starting_hash, i);
@@ -56,29 +57,33 @@ pub fn solve(starting_hash: &[u8], start: u32) -> Option<(u32, u32)> {
         let offset: u32 = (e >> 32).try_into().unwrap();
         let pos: u32 = offset % BOARD_SIZE;
 
+        board.push((i, p, pos));
+
         if p == Piece::KING {
             last_king = Some((pos, i));
-            threats.clear(); 
-            
-            for &(nonce, piece, piece_pos) in &board {
-                if piece != Piece::KING && is_solved(row_size, pos, piece_pos, piece) {
+        }
+
+        if let Some((current_king_pos, current_king_nonce)) = last_king {
+            // Simulate BTreeMap overwrite by only considering the latest piece at each position
+            let mut threats: Vec<u32> = Vec::new();
+            let mut visited_pos: HashSet<u32> = HashSet::new();
+
+            for &(nonce, piece, piece_pos) in board.iter().rev() {
+                // If we've already processed a newer piece at this position, skip
+                if visited_pos.contains(&piece_pos) {
+                    continue;
+                }
+                visited_pos.insert(piece_pos);
+
+                // Check if this (latest) piece at piece_pos threatens the king
+                if nonce != current_king_nonce && is_solved(row_size, current_king_pos, piece_pos, piece) {
                     threats.push(nonce);
                 }
             }
-        } 
-        else if let Some((last_king_pos, _)) = last_king {
-            if is_solved(row_size, last_king_pos, pos, p) {
-                threats.push(i);
-            }
-        }
-        
-        board.push((i, p, pos));
 
-        if let Some((_, last_king_nonce)) = last_king {
             if threats.len() >= CHECKS_NEEDED as usize {
-                if !threats.contains(&last_king_nonce) {
-                    threats.push(last_king_nonce);
-                }
+                threats.push(current_king_nonce);
+
                 if let Some(first_threat) = threats.iter().min() {
                     return Some((*first_threat, i));
                 }
@@ -108,9 +113,9 @@ mod test {
     use proptest::prelude::*;
 
     proptest! {
+        #![proptest_config(ProptestConfig { cases: 5000, ..Default::default() })]
         #[test]
         fn test_solve(starting_hash in prop::array::uniform32(any::<u8>())) {
-            
             if let Some((e_l, e_h)) = solve(&starting_hash, 0) {
                 
                 if let Some((t_l, t_h)) = solve(&starting_hash, e_l) {
@@ -120,14 +125,13 @@ mod test {
                 }
 
                 if let Some((p_l, p_h)) = prover::default_solve(&starting_hash, e_l) {
-                     assert_eq!(
+                    assert_eq!(
                         (e_l, e_h), (p_l, p_h),
                         "user contract inconsistent with reference. {e_l} != {p_l} or {e_h} != {p_h}"
-                     );
+                    );
                 } else {
-                     panic!("Reference solve failed where user solve succeeded");
+                    panic!("Reference solve failed where user solve succeeded");
                 }
-            } else {
             }
         }
     }
